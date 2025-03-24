909cd2efbbb780dfa5350bc74b618429e81cf0e0
BrunoEstevamRJ <bruno.estevam@outlook.com.br>
2025-03-24 19:44:13 -0300

    FEAT - criação da base e melhorias nos templates e links, e urls,

--- diamante_web/urls.py
+++ diamante_web/urls.py
@@ -1,9 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from meu_app import views
 
 urlpatterns = [
-    
+    path('', views.page_web, name='home'),
     path('admin/', admin.site.urls),
-    path('meu_app/', include('meu_app.urls'))
+    path('meu_app/', include('meu_app.urls')),
+    path('contato/', views.contato, name='contato'),
 ]
--- meu_app/views.py
+++ meu_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
+
+# Initial Page
 def page_web(request):
     return render(request, 'index.html', {'nome': 'Wescley'})
+
+
+# Contato  Page
+def contato(request):
+    return render(request, 'contato.html')
