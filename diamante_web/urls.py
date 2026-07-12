d2c05ef9f44bdea2fa3d913f7de78532f1e87a0d
Bruno Estevam <67297226+BrunoEstevamRJ@users.noreply.github.com>
2026-07-12 00:08:09 -0300

    Add home view to URL patterns

--- diamante_web/urls.py
+++ diamante_web/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from meu_app import views
 
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
-    path('meu_app/', include('meu_app.urls'))
+    path('', views.page_web, name='home'),
+    path('meu_app/', include('meu_app.urls')),
 ]
